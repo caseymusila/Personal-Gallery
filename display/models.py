3accb32abd675b849a9bbf492ff0e8bcb689a234
caseymusila <musilacasey@gmail.com>
2021-09-06 09:08:52 +0300

    we create a class for location and pass in the parameters

--- display/models.py
+++ display/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
-class Images(models.Model):
-    image = CloudinaryField('image')
-    image_name = models.CharField(max_length = 100)
-    image_description = models.TextField()
-    image_category = models.ManyToManyField(Category, related_name = 'posts')
-    image_location = models.ForeignKey(Location, related_name = 'posts', on_delete = models.CASCADE)
-    created = models.DateTimeField(auto_now_add = True)
-    last_modified = models.DateTimeField(auto_now = True)
+class Location(models.Model):
+    location = models.CharField(max_length = 50)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.location
