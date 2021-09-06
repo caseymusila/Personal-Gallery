90dafc0407465a0c02db02fd767ad96048079c84
caseymusila <musilacasey@gmail.com>
2021-09-06 10:30:29 +0300

    we create the path for the category and pass it through

--- display/urls.py
+++ display/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home),
     path('<int:pk>/', views.image_details, name='details'),
     path('search/', views.search_results, name = 'search_results'),
+    path('<category>/', views.image_category, name = 'images_category'),
 ]
