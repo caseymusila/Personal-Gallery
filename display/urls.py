10331fba0eca99b07139294b605e12b622939a12
caseymusila <musilacasey@gmail.com>
2021-09-06 10:21:51 +0300

    we pass in the path for the url of search

--- display/urls.py
+++ display/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('<int:pk>/', views.image_details, name='details'),
+    path('search/', views.search_results, name = 'search_results'),
 ]
