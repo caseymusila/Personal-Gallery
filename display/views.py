9fe94b239567fc75fb45a4ea0261095a34ae302d
caseymusila <musilacasey@gmail.com>
2021-09-06 05:39:19 +0300

    we create a path to our homepage

--- display/views.py
+++ display/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 from .models import Images, Location, Category
 
 # Create your views here.
+def home(request):
+    '''
+    Function to display the home page
+    '''
+    images = Images.objects.all().order_by('-last_modified')
+    location = Location.objects.all()
+    context = {
+        'images': images,
+        'location': location
+    }
+    return render(request, "all-images/images.html", context)
